a913c9acc03d8a4be9256622ad29056cae7e82ac
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-01 09:56:15 +0330

    styling & done!

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -12,16 +12,21 @@ const App = () => {
   }
 
   return (
-    <div>
-      <input className='' 
+    <div className='flex flex-col gap-4 items-center justify-center'>
+     <h1 className='text-3xl font-bold text-pink-600 mt-12'>QR Code Generator</h1>
+     <div className='flex flex-col gap-4'>
+      <input className='border-gray-400 border rounded-md px-4 py-2' 
       type='text' 
-      placeholder='Enter Your Name Or Anything!' 
+      placeholder='Enter Your Name' 
       value={input} 
       onChange={(e) => setInput(e.target.value)}/>
       <button disabled={input && input.trim !== '' ? false : true} 
       onClick={handleClick} 
-      className=''>Generate</button>
-      <QRCode value={qrCode} size={40}/>
+      className='bg-gray-700 hover:bg-black text-white rounded-md my-2 px-4 py-2'>Generate</button>
+      </div>
+      <div>
+      <QRCode value={qrCode} size={400}/>
+      </div>
     </div>
   )
 }
